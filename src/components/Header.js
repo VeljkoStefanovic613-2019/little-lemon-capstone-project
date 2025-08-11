656821a646ae58e6e1291387a1e85ba7510e0a9c
VeljkoStefanovic613-2019 <85968664+VeljkoStefanovic613-2019@users.noreply.github.com>
2025-08-11 02:26:56 +0200

    Added Header.js, tailwind and reactrouter

---
+++ src/components/Header.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import logo from "../assets/logo1.jpg"
+
+export const Header = () => {
+const [IsMenuOpen, SetIsMenuOpen] = useState(false);
+
+  return (
+    
+
+<nav className="bg-white dark:bg-secondary fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+      <img src={logo} className="h-12 " alt="Flowbite Logo" />
+  </a>
+  <div className="flex  space-x-3 md:space-x-0 rtl:space-x-reverse">
+      <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" onClick={() => SetIsMenuOpen(!IsMenuOpen)}  aria-controls="navbar-sticky" aria-expanded={IsMenuOpen} >
+        <span className="sr-only">Open main menu</span>
+        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+  </div>
+  <div  className={`items-center justify-between w-full md:flex md:w-auto ${IsMenuOpen ? '' : 'hidden'}`} id="navbar-sticky">
+    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-secondary dark:border-gray-700">
+      <li>
+        <a href="#" className="block py-2 px-3 text-white bg-primary rounded-sm md:bg-transparent md:text-primary md:p-0 md:dark:text-primary" aria-current="page">Home</a>
+      </li>
+      <li>
+        <a href="#" className="block py-2 px-3 text-secondary rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-primary dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+      </li>
+      <li>
+        <a href="#" className="block py-2 px-3 text-secondary rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-primary dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Menu</a>
+      </li>
+      <li>
+        <a href="#" className="block py-2 px-3 text-secondary rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Reservations</a>
+      </li>
+      <li>
+        <a href="#" className="block py-2 px-3 text-secondary rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Order Online</a>
+      </li>
+      <li>
+        <a href="#" className="block py-2 px-3 text-secondary rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Login</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
+
+  )
+}
